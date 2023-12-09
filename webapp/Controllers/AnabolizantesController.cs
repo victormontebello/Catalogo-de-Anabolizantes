f09b2a2cb62d60f731b1165c430b686d643e051c
victor montebello <montebellovictor@gmail.com>
2023-12-09 09:21:54 -0300

    Controller e http getAll

---
+++ webapp/Controllers/AnabolizantesController.cs
@@ -0,0 +1,28 @@
+﻿using domain.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnabolizantesController : ControllerBase
+    {
+        private IAnabolizanteRepositorio _repositorio;
+
+        public AnabolizantesController(IAnabolizanteRepositorio repositorio)
+        {
+            _repositorio = repositorio;        
+        }
+
+        [HttpGet]
+        public IActionResult ObterTodos()
+        {
+            var anabolizantes = _repositorio.GetAll();
+            return Ok(anabolizantes);    
+        }
+       
+
+
+
+    }
+}
