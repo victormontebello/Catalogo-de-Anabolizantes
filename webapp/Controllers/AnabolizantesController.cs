4fcd613868a98d1caff5c2543bd97d03d1d44e89
victor montebello <montebellovictor@gmail.com>
2023-12-09 09:37:08 -0300

    endpoints

--- webapp/Controllers/AnabolizantesController.cs
+++ webapp/Controllers/AnabolizantesController.cs
@@ -1,4 +1,6 @@
-﻿using domain.Interface;
+﻿using domain.Entities;
+using domain.Interface;
+using FluentMigrator.Builders.Alter.Column;
 using Microsoft.AspNetCore.Mvc;
 
 namespace webapp.Controllers
@@ -11,18 +13,58 @@ namespace webapp.Controllers
 
         public AnabolizantesController(IAnabolizanteRepositorio repositorio)
         {
-            _repositorio = repositorio;        
+            _repositorio = repositorio;
         }
 
         [HttpGet]
         public IActionResult ObterTodos()
         {
             var anabolizantes = _repositorio.GetAll();
-            return Ok(anabolizantes);    
+            return Ok(anabolizantes);
         }
-       
 
+        [HttpGet("{id}")]
+        public IActionResult ObterPeloId([FromRoute] int id)
+        {
+            if (id == 0) { return BadRequest(); }
+            var bomba = _repositorio.GetById(id);
 
+            if (bomba == null) { return NotFound("Essa bomba ai nao existe"); }
 
+            return Ok(bomba);
+        }
+
+        [HttpPost]
+        public IActionResult Criar([FromBody] Anabolizante novaBomba)
+        {
+            if (novaBomba == null) { return BadRequest(); }
+            _repositorio.Insert(novaBomba);
+            return Created($"novaBomba/{novaBomba}", novaBomba);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar([FromRoute] int id, [FromBody] Anabolizante al)
+        {
+            if (id == 0) { return NotFound(); }
+            if (al == null) { return BadRequest(); }
+
+            al.Id = id;
+            _repositorio.Update(al);
+
+            return Ok(al);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remover([FromRoute]int id)
+        {
+            if (id == 0) { return NoContent(); }
+            var bomba = _repositorio.GetById(id);
+
+            if (bomba == null) { return NotFound($"Bomba com esse codigo ai nao existe"); }
+            _repositorio.Delete(id);
+
+            return Ok();
+
+        }
     }
 }
